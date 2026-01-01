29d1ac5d94aa80d947469fe174319413702ba203
orbitallinux <sumtas5555@gmail.com>
2026-01-01 14:52:20 +0300

    Add files via upload

--- build.sh
+++ build.sh
@@ -2,21 +2,49 @@
 
 KERNEL_VERSION=6.1.159
 TOYBOX_VERSION=0.8.13
+MUSL_LIBC_VERSION=1.2.5
 
 mkdir -p src
 cd src
 
-# Toybox
-
 wget https://landley.net/toybox/downloads/toybox-$TOYBOX_VERSION.tar.gz
 tar xf toybox-$TOYBOX_VERSION.tar.gz
 
+wget https://musl.libc.org/releases/musl-$MUSL_LIBC_VERSION.tar.gz
+tar -xf musl-$MUSL_LIBC_VERSION.tar.gz
+
+wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$KERNEL_VERSION.tar.xz
+tar -xf linux-$KERNEL_VERSION.tar.xz
+
+mkdir -p "../output/initrd"
+
+cd ../output/initrd
+mkdir dev sys etc proc usr/share usr/lib usr/lib64 etc/init.d lib lib64
+cd ../../src
+
+# Musl C library
+
+cd musl-$MUSL_LIBC_VERSION
+
+./configure
+
+make || exit 1
+
+cp lib/libc.so ../../output/initrd/lib/ld-musl-x86_64.so.1
+
+cd ..
+
+# Toybox
+
 cd toybox-$TOYBOX_VERSION
 
-mkdir -p "../../output/initrd"
-
 make defconfig
-make || exit
+
+cp ../../.config .config
+
+yes "" | make oldconfig
+
+make LDFLAGS=--static || exit 1
 make PREFIX=../../output/initrd install
 
 cd ..
@@ -25,22 +53,25 @@ cd ..
 
 cd ../output/initrd
 
-echo '#!/bin/sh' > init
-#echo 'mount -t sysfs sysfs /sys' >> init
-#echo 'mount -t proc proc /proc' >> init
-#echo 'mount -t devtmpfs udev /dev' >> init
-#echo 'sysctl -w kernel.printk="2 4 1 7"'
-echo '/bin/sh' >> init
+ln -s sbin/init init
 
-chmod +x init
+cd etc/init.d
+
+echo '#!/bin/sh' > rcS
+echo 'mount -t sysfs /sys /sys' >> rcS
+echo 'mount -t proc /proc /proc' >> rcS
+echo 'mount -t devtmpfs /dev /dev' >> rcS
+
+chmod +x rcS
+
+cd ../..
 
 find . | cpio -o -H newc > ../init.cpio
 
 cd ../../src
 
 # Kernel
-wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$KERNEL_VERSION.tar.xz
-tar -xf linux-$KERNEL_VERSION.tar.xz
+
 cd linux-$KERNEL_VERSION
 
 make defconfig
@@ -55,10 +86,12 @@ make defconfig
 # End of changing kernel configurations
 
 yes "" | make oldconfig
-make || exit
+make isoimage FDARGS="initrd=/init.cpio" FDINITRD=../../output/init.cpio || exit 1
 
-cp arch/x86/boot/bzImage ../../output/bzImage
+#cp arch/x86/boot/bzImage ../../output/bzImage
+
+cp arch/x86/boot/image.iso ../../output/image.iso
 
 cd ..
 
-qemu-system-x86_64 -kernel ../output/bzImage -initrd ../output/init.cpio
+qemu-system-x86_64 -cdrom ../output/image.iso
